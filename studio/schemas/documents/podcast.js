06c22294f6cfe68f0f145e10d86f0969e91cf6d8
Eoin <debatebroshow@gmail.com>
2020-09-01 06:13:16 -0400

    added some initial values

--- studio/schemas/documents/podcast.js
+++ studio/schemas/documents/podcast.js
@@ -12,6 +12,74 @@ export default {
       type: 'string'
     },
     {
+      title: 'SoundCloud Link',
+      name: 'href',
+      type: 'url',
+      validation: Rule => Rule.uri({
+        scheme: ['https']
+      })
+    },
+    {
+      title: 'SoundCloud Embed Width',
+      name: 'parameters',
+      type: 'array',
+      of: [{
+        type: 'integer'
+      }],
+      options: {
+        layout: 'radio',
+        list: [{
+            title: '300',
+            value: 300
+          },
+          {
+            title: '500',
+            value: 500
+          },
+          {
+            title: '650',
+            value: 650
+          },
+        ]
+      }
+    },
+    {
+      title: 'SoundCloud Options',
+      name: 'parameters',
+      type: 'array',
+      of: [{
+        type: 'string'
+      }],
+      options: {
+        layout: 'radio',
+        list: [{
+            title: 'Autoplay',
+            value: 'autoplay'
+          },
+          {
+            title: 'Buying',
+            value: 'buying'
+          },
+          {
+            title: 'Sharing',
+            value: 'sharing'
+          },
+          {
+            title: 'Download',
+            value: 'download'
+          },
+          {
+            title: 'Show Playcount',
+            value: 'show_playcount'
+          },
+          {
+            title: 'Show User',
+            value: 'show_user'
+          }
+        ]
+      }
+    },
+    {
       name: 'slug',
       title: 'Slug',
       type: 'slug',
@@ -68,24 +136,32 @@ export default {
       }]
     }
   ],
-  preview: {
-    select: {
-      title: 'title',
-      publishedAt: 'publishedAt',
-      slug: 'slug'
+  initialValue: {
+    parameters: ['False',
+      'False',
+      'True',
+      'True',
+      'True',
+      'True']
     },
-    prepare({
-      title = 'No title',
-      publishedAt,
-      slug = {},
-      media
-    }) {
-      const dateSegment = format(publishedAt, 'YYYY/MM')
-      const path = `/${dateSegment}/${slug.current}/`
-      return {
-        title,
-        subtitle: publishedAt ? path : 'Missing publishing date'
+    preview: {
+      select: {
+        title: 'title',
+        publishedAt: 'publishedAt',
+        slug: 'slug'
+      },
+      prepare({
+        title = 'No title',
+        publishedAt,
+        slug = {},
+        media
+      }) {
+        const dateSegment = format(publishedAt, 'YYYY/MM')
+        const path = `/${dateSegment}/${slug.current}/`
+        return {
+          title,
+          subtitle: publishedAt ? path : 'Missing publishing date'
+        }
       }
     }
   }
-}
